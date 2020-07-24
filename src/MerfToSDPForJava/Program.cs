64c731623f278c04305d7d8f1db1b5400dbc1257
surf zhang <v-haiboz@microsoft.com>
2020-07-24 12:24:59 +0800

    DEV TASK 238575:[Java][SDP] Prepare script to convert Merf to SDP for doxygen pipeline result (doxygen) Project init

---
+++ src/MerfToSDPForJava/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MerfToSDPForJava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

1587389eff332338eb2c846b2781f58bf9fd01b9
surf zhang <v-haiboz@microsoft.com>
2020-07-24 14:02:54 +0800

    #Task 238575:[Java][SDP] Prepare script to convert Merf to SDP for doxygen pipeline result (doxygen)
    Create DataContracts

---
+++ src/Microsoft.Content.Build.MerfToSDPForJava.SDP.DataContracts/ItemType.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.Content.Build.MerfToSDPForJava.SDP.DataContracts
+{
+    using System;
+    public enum ItemType
+    {
+        Default,
+        Toc,
+        Assembly,
+        Namespace,
+        Class,
+        Interface,
+        Struct,
+        Delegate,
+        Enum,
+        Field,
+        Property,
+        Event,
+        Constructor,
+        Method,
+        Operator,
+        Container,
+        AttachedEvent,
+        AttachedProperty
+    }
+}
---
+++ src/Microsoft.Content.Build.MerfToSDPForJava.SDP.DataContracts/NamespaceSDPModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.Content.Build.MerfToSDPForJava.SDP.DataContracts
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using YamlDotNet.Serialization;
+    public class NamespaceSDPModel: AbstractSDPModelBase
+    {
+        [JsonIgnore]
+        [YamlIgnore]
+        public override string YamlMime { get; } = "YamlMime:JavaPackage";
+
+        [JsonProperty("classes")]
+        [YamlMember(Alias = "classes")]
+        public IEnumerable<string> Classes { get; set; }
+
+        [JsonProperty("enums")]
+        [YamlMember(Alias = "enums")]
+        public IEnumerable<string> Enums { get; set; }
+
+        [JsonProperty("interfaces")]
+        [YamlMember(Alias = "interfaces")]
+        public IEnumerable<string> Interfaces { get; set; }
+    }
+}

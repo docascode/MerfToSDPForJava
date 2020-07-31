52763992e2e01424aee7f9760136835682174fcd
haibo zhang <66710086+v-haiboz@users.noreply.github.com>
2020-07-31 15:10:57 +0800

    Feature/comet (#7)
    * #Task 238575: interpret Namespace,Type(Class&Interface) and Enum, then it was saved into related yml files.
    
    * #Task 238575: rename method's name.
    
    * #Task:238575: Optimize Code
    
    * #Task 238575:
    1.add logic to interpret Method,Constructor and Field,.
    2.Formatting code

---
+++ src/Microsoft.Content.Build.MerfToSDPForJava.Interpreter/MethodExpression.cs
@@ -0,0 +1,82 @@
+﻿namespace Microsoft.Content.Build.MerfToSDPForJava.Interpreter
+{
+    using Microsoft.Content.Build.MerfToSDPForJava.Common;
+    using Microsoft.Content.Build.MerfToSDPForJava.DataContracts.ManagedReference;
+    using Microsoft.Content.Build.MerfToSDPForJava.DataContracts.SDP;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MethodExpression : AbstractExpression
+    {
+        private Dictionary<string, MemberSDPModel> keyValuePairs_methodOverloading = new Dictionary<string, MemberSDPModel>();
+        public MethodExpression(string outputFolder, string fileName)
+               : base(outputFolder, fileName)
+        {
+
+        }
+        public override bool Interpret(PageModel pageModel, BuildContext context)
+        {
+            if (pageModel == null || pageModel.Items == null)
+            {
+                return false;
+            }
+
+            var methods = pageModel.Items.Where(item => item.Type == MemberType.Method)?.ToList();
+            if (methods == null || methods.Count == 0)
+                return true;
+
+            var methodsOverLoading = methods.Where(item => item != null && item.Overload != null && item.Overload.EndsWith('*'))?.ToList();
+            if (methodsOverLoading != null)
+            {
+                return true;
+            }
+
+            foreach (var item in methodsOverLoading)
+            {
+                var filename = item.Overload.Remove(item.Overload.Length - 1);
+
+                MemberSDPModel exist;
+                if (!keyValuePairs_methodOverloading.TryGetValue(filename, out exist))
+                {
+                    var memberSDPModel = AutoMapArticleItemYamlToMemberSDPModel(item);
+                    memberSDPModel.Uid = item.Overload;
+                    keyValuePairs_methodOverloading.Add(filename, memberSDPModel);
+                }
+                else
+                {
+                    var member = TransferMember(item);
+                    exist.Members.Add(member);
+                }
+            }
+
+            foreach (var keyValuePair in keyValuePairs_methodOverloading)
+            {
+                base.Save(keyValuePair.Value, keyValuePair.Value.YamlMime, keyValuePair.Key);
+            }
+
+            return true;
+        }
+        private MemberSDPModel AutoMapArticleItemYamlToMemberSDPModel(ArticleItemYaml articleItemYaml)
+        {
+            if (articleItemYaml == null)
+                return null;
+
+            var memberSDPModel = new MemberSDPModel();
+            memberSDPModel.FullName = articleItemYaml.FullName;
+            memberSDPModel.Name = articleItemYaml.Name;
+            memberSDPModel.NameWithType = articleItemYaml.NameWithType;
+            memberSDPModel.Uid = articleItemYaml.Uid;
+            memberSDPModel.Type = articleItemYaml.Type.ToString().ToLower();
+            memberSDPModel.Package = articleItemYaml.PackageName;
+            memberSDPModel.Members = new List<Member>();
+
+            var member = TransferMember(articleItemYaml);
+            if (member != null)
+            {
+                memberSDPModel.Members.Add(member);
+            }
+
+            return memberSDPModel;
+        }
+    }
+}
--- src/Microsoft.Content.Build.MerfToSDPForJava.Interpreter/TypeExpress.cs
+++ src/Microsoft.Content.Build.MerfToSDPForJava.Interpreter/TypeExpress.cs
@@ -5,6 +5,7 @@
     using Microsoft.Content.Build.MerfToSDPForJava.DataContracts.SDP;
     using System.Collections.Generic;
     using System.Linq;
+
     public class TypeExpress : AbstractExpression
     {
         public TypeExpress(string outputFolder, string fileName)
@@ -27,7 +28,6 @@
                 objType.Constructors = TransferConstructors(pageModel.Items);
                 objType.Fields = TransferFields(pageModel.Items);
                 objType.Methods = TransferMethods(pageModel.Items);
-
                 objType.FullName = typeItem.FullName;
                 objType.Inheritances = ConvertStringToInlineMD(typeItem.Inheritance);
                 objType.InheritedMembers = typeItem.InheritedMembers;
@@ -42,6 +42,17 @@
 
                 base.Save(objType, objType.YamlMime, objType.Uid);
 
+                List<AbstractExpression> expressions = new List<AbstractExpression>();
+                expressions.Add(new ConstructorExpression(_outputFolder, objType.Uid));
+                expressions.Add(new FieldExpression(_outputFolder, objType.Uid));
+                expressions.Add(new MethodExpression(_outputFolder, objType.Uid));
+                foreach (var expression in expressions)
+                {
+                    if (!expression.Interpret(pageModel, context))
+                    {
+                        break;
+                    }
+                }
 
                 return false;
             }
@@ -52,9 +63,9 @@
         {
             if (articleItemYamls == null)
                 return null;
-            var constructors = articleItemYamls.Where(item => item.Type == MemberType.Constructor)?.Select(p => p.Uid)?.ToArray();
 
-            if (constructors ==null || constructors.Length == 0)
+            var constructors = articleItemYamls.Where(item => item.Type == MemberType.Constructor)?.Select(p => p.Uid)?.ToArray();
+            if (constructors == null || constructors.Length == 0)
             {
                 return null;
             }
@@ -65,8 +76,8 @@
         {
             if (articleItemYamls == null)
                 return null;
-            var fields = articleItemYamls.Where(item => item.Type == MemberType.Field)?.Select(p => p.Uid)?.ToArray();
 
+            var fields = articleItemYamls.Where(item => item.Type == MemberType.Field)?.Select(p => p.Uid)?.ToArray();
             if (fields == null || fields.Length == 0)
             {
                 return null;
@@ -78,8 +89,8 @@
         {
             if (articleItemYamls == null)
                 return null;
-            var methods = articleItemYamls.Where(item => item.Type == MemberType.Method)?.Select(p => p.Uid)?.ToArray();
 
+            var methods = articleItemYamls.Where(item => item.Type == MemberType.Method)?.Select(p => p.Uid)?.ToArray();
             if (methods == null || methods.Length == 0)
             {
                 return null;

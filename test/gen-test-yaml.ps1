7a7db730c72962f61a3ca22b6f62762d6e28daa8
Ann Meng <anmeng@microsoft.com>
2020-10-20 13:56:01 +0800

    correct path in printed message

--- test/gen-test-yaml.ps1
+++ test/gen-test-yaml.ps1
@@ -6,4 +6,4 @@ $repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
 
 & "$repoRoot/target/Release/MerfToSDPForJava/netcoreapp3.1/MerfToSDPForJava.exe" -s $repoRoot\test\docs-ref-autogen -o $repoRoot\test\docs-ref-autogen-sdp-after
 
-Write-Host "Finished. Yamls generated in $repoRoot\test\docs-ref-autogen_sdp_after"
+Write-Host "Finished. Yamls generated in $repoRoot\test\docs-ref-autogen-sdp-after"

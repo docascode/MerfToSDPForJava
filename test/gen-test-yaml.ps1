d4e701d0ef9cb01bf1b777d5c888db759c95dfb7
Ann Meng <anmeng@microsoft.com>
2020-10-20 13:40:45 +0800

    fix test script target path

--- test/gen-test-yaml.ps1
+++ test/gen-test-yaml.ps1
@@ -4,6 +4,6 @@ Write-Host "Start generating yamls for test ..."
 
 $repoRoot = $($MyInvocation.MyCommand.Definition) | Split-Path | Split-Path
 
-& "$repoRoot/src/MerfToSDPForJava/bin/Release/netcoreapp3.1/MerfToSDPForJava.exe" -s $repoRoot\test\docs-ref-autogen -o $repoRoot\test\docs-ref-autogen-sdp-after
+& "$repoRoot/target/Release/MerfToSDPForJava/netcoreapp3.1/MerfToSDPForJava.exe" -s $repoRoot\test\docs-ref-autogen -o $repoRoot\test\docs-ref-autogen-sdp-after
 
 Write-Host "Finished. Yamls generated in $repoRoot\test\docs-ref-autogen_sdp_after"
